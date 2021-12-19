0a290f311ed03b18e290501ca1c489cacf524030
Kirill Romanenko <brs.group.com@gmail.com>
2021-12-19 16:54:02 +0200

    hw2 with getById bug

--- model/index.js
+++ model/index.js
@@ -1,15 +1,51 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises')
+const { v4 } = require('uuid')
+const contactsPath = require('./contactsPath')
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath)
+  const contacts = JSON.parse(data)
+  return contacts
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (id) => {
+  const contacts = await listContacts()
+  const data = contacts.find(contact => String(contact.id) === id)
+  if (!data) {
+    return null
+  }
+  return data
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (id) => {
+  const contacts = await listContacts()
+  const idx = contacts.findIndex((item) => String(item.id) === id)
+  if (idx === -1) {
+    return null
+  }
+  const removedContact = contacts.splice(idx, 1)
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+  return removedContact
+}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const newContact = { id: v4(), ...body }
+  const contacts = await listContacts()
+  contacts.push(newContact)
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+  return newContact
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (id, body) => {
+  const contacts = await listContacts()
+  const idx = contacts.findIndex((item) => String(item.id) === id)
+  if (idx === -1) {
+    return null
+  }
+  contacts[idx] = { id: id, ...body }
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+  return contacts[idx]
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,89 @@
 const express = require('express')
 const router = express.Router()
+const moment = require('moment')
+const fs = require('fs/promises')
+const { NotFound, BadRequest } = require('http-errors')
+const Joi = require('joi')
+const contactsController = require('../../model/index.js')
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const joiSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.use(async(req, res, next) => {
+  const { method, url } = req
+  const { statusCode } = res
+  const date = moment().format('DD.MM.YYYY_hh:mm:ss')
+  const str = `\n${method} ${url} ${date} ${statusCode}`
+  await fs.appendFile('server.log', str)
+  next()
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    const contacts = await contactsController.listContacts()
+    res.json(contacts)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params
+  try {
+    const contact = contactsController.getContactById(id)
+    if (!contact) {
+      throw new NotFound()
+    }
+    res.json(contact)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = joiSchema.validate(req.body)
+    if (error) {
+      throw new BadRequest(error.message)
+    }
+    const newContact = await contactsController.addContact(req.body)
+    res.status(201).json(newContact)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params
+  try {
+    const deleteContact = await contactsController.removeContact(id)
+    if (!deleteContact) {
+      throw new NotFound()
+    }
+    res.json({ message: 'contact deleted' })
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { error } = joiSchema.validate(req.body)
+    if (error) {
+      throw new BadRequest('message: missing fields')
+    }
+    const { id } = req.params
+    const updateContact = await contactsController.updateContact(id, req.body)
+    if (!updateContact) {
+      throw new NotFound()
+    }
+    res.json(updateContact)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
